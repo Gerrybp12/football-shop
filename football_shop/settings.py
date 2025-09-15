fe359348ddba9d114fbae36e9f2621a78cd348fb
Gerrybp12 <gerry.bimaputra@gmail.com>
2025-09-15 13:46:49 +0700

    add csrf trusted origin

--- football_shop/settings.py
+++ football_shop/settings.py
@@ -32,6 +32,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "gerry-bima-footballshop.pbp.cs.ui.ac.id"]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://gerry-bima-footballshop.pbp.cs.ui.ac.id"
+]
+
 
 # Application definition
 
