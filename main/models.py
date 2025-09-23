179b2e99e89fc8bd6d53e2478c86d08925764ca5
Gerrybp12 <gerry.bimaputra@gmail.com>
2025-09-23 13:55:41 +0700

    pengumpulan tugas 4

--- main/models.py
+++ main/models.py
@@ -1,12 +1,15 @@
 import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Product(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
     thumbnail = models.URLField()
     category = models.CharField()
     is_featured = models.BooleanField()
+    
